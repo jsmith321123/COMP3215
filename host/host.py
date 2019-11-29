cd0f8bf7319a342316cd8d5eca821cdb4454ceb8
jsmith321123 <joshuaellissmith26@gmail.com>
2019-11-29 15:38:46 +0000

    serial testing with flask implementation of webserver

--- host/host.py
+++ host/host.py
@@ -1,14 +1,32 @@
-import os, pty, serial
+from flask import Flask, render_template, redirect, url_for
+import serial
 
-import os, pty, serial
+app = Flask(__name__)
 
-master, slave = pty.openpty()
-s_name = os.ttyname(slave)
+ser = serial.Serial('COM3')
 
-ser = serial.Serial(s_name)
 
-# To Write to the device
-ser.write('Your text')
+@app.route("/")
+def index():
+    return render_template("index.html", message="none")
 
-# To read from the device
-os.read(master, 1000)
+
+@app.route("/lights-on")
+def lights_on():
+    print("lights on")
+
+    return redirect(url_for("index"))
+
+
+@app.route("/lights-off")
+def lights_off():
+    print("lights off")
+
+    return redirect(url_for("index"))
+
+
+if __name__ == '__main__':
+    while True:
+        print(ser.read())
+
+    app.run()
